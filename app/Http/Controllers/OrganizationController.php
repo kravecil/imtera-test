640682f017e7c9420dc6f124cf63d51915dcf1d3
kravecil <brightsky@yandex.ru>
2026-06-14 00:23:30 +0500

    feat: Реализована валидация ссылки Организации

--- app/Http/Controllers/OrganizationController.php
+++ app/Http/Controllers/OrganizationController.php
@@ -10,7 +10,12 @@ class OrganizationController extends Controller
     public function store(Request $request): Organization
     {
         $validated = $request->validate([
-            'link' => ['required', 'string', 'unique:organizations,link',]
+            'link' => [
+                'required',
+                'string',
+                'unique:organizations,link',
+                'regex:/https:\/\/yandex.ru\/maps\/-\/[A-Za-z0-9_-]{8}/i'
+            ]
         ]);
 
         return Organization::create($validated);
