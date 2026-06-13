c69967fe69b3bf954d065fbdf1b9e38ed4a0fd15
kravecil <brightsky@yandex.ru>
2026-06-13 23:59:18 +0500

    feat(routes): Добавлен эндпоинт создания Организации

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\OrganizationController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/organizations', [OrganizationController::class, 'store']);
+});
