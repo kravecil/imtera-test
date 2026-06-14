c9ec5dd099dc0833fb5b0a0d0b303ae4c93ee27f
kravecil <brightsky@yandex.ru>
2026-06-14 14:32:19 +0500

    feat(console): Добавлена команда для разового запуска парсинга по URL

---
+++ app/Console/Commands/ParseOrganization.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+use \App\Services\ParsingService;
+
+#[Signature('parse:organization {url}')]
+#[Description('Загрузить сведения об организации по ссылке')]
+class ParseOrganization extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle(ParsingService $service)
+    {
+        $url = $this->argument('url');
+
+        $service->parse($url);
+
+        return 0;
+    }
+}
