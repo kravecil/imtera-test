0b999fc20240c1e10d922d2045deba3d9c66fd72
kravecil <brightsky@yandex.ru>
2026-06-15 15:55:21 +0500

    feat(parsing): Реализован парсинг отзывов

--- app/Services/ParsingService.php
+++ app/Services/ParsingService.php
@@ -10,6 +10,7 @@ use Playwright\Playwright;
 use Playwright\Browser\BrowserContext;
 use Playwright\Page\Page;
 use App\Utils\UrlParser;
+use Carbon\Carbon;
 
 class ParsingService
 {
@@ -20,13 +21,15 @@ class ParsingService
     private ?int $ratesCount = null;
     private ?int $reviewsCount = null;
 
+    private array $reviews = [];
+
     private ?BrowserContext $browser = null;
     private ?Page $page = null;
 
     private function initPlaywright(): void
     {
         $this->browser = Playwright::chromium([
-            'headless' => false,
+            'headless' => true,
             'args' => [
                 '--disable-blink-features=AutomationControlled',
                 '--no-sandbox',
@@ -38,6 +41,8 @@ class ParsingService
 
     private function initPage(): void
     {
+        Carbon::setLocale('ru');
+
         $this->page = $this->browser->newPage([
             'viewport' => ['width' => 1920, 'height' => 1080],
             'userAgent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
@@ -71,7 +76,7 @@ class ParsingService
 
         $url = self::YANDEX_MAP_BASE_URL . "/{$organizationSlug}/{$organizationId}";
 
-        logger("Начинаем парсинг ссылки [{$url}]");
+        logger("Начинаем парсингобщих данных [{$url}]");
 
         $this->initPlaywright();
         $this->initPage();
@@ -157,5 +162,104 @@ class ParsingService
     {
         $normalized = str_replace(',', '.', preg_replace('/[^\d,\.]/', '', $text));
         return filter_var($normalized, FILTER_VALIDATE_FLOAT) !== false ? (float) $normalized : null;
+    }
+
+    public function parseReviews(string $url): void
+    {
+        ['slug' => $organizationSlug, 'id' => $organizationId] = UrlParser::parseYandexMapsOrgUrl($url);
+
+        $url = self::YANDEX_MAP_BASE_URL . "/{$organizationSlug}/{$organizationId}/reviews";
+
+        logger("Начинаем парсинг отзывов [{$url}]");
+
+        $this->initPlaywright();
+        $this->initPage();
+
+        try {
+            $this->page?->goto($url, ['waitUntil' => 'load']);
+
+            $this->page?->waitForSelector('div.card-reviews-view');
+
+            $this->loadAndExtractReviewsData();
+        } catch (Exception $e) {
+            $message = "Ошибка при парсинге {$url}: " . $e->getMessage();
+            Log::error($message, ['exception' => $e]);
+            throw new Exception($message, 0, $e);
+        } finally {
+            $this->tearDown();
+        }
+    }
+
+    private function loadAndExtractReviewsData(): void
+    {
+        $this->loadAllReviews();
+        $this->processAllReviews();
+    }
+
+    private function loadAllReviews(): void
+    {
+        $reviewLocator = $this->page->locator('.business-reviews-card-view__review');
+
+        $this->page->mouse()->move(0, 0);
+
+        $retries = 0;
+        $count = $reviewLocator->count();
+        $prevCount = $count;
+        for ($i = 0; $i < 20; $i++) {
+            $this->page->mouse()->wheel(0, 50000);
+            $count = $reviewLocator->count();
+            Log::info("Получено отзывов: $count");
+
+            if ($prevCount == $count) {
+                $retries++;
+                if ($retries > 3) break;
+            } else {
+                $prevCount = $count;
+            }
+
+            usleep(500000);
+        }
+    }
+
+    private function processAllReviews(): void
+    {
+        try {
+            $reviewsLocator = $this->page->locator('.business-reviews-card-view__review');
+            $reviewsCount = $reviewsLocator->count();
+
+            for ($i = 0; $i < $reviewsCount; $i++) {
+                $review = $reviewsLocator->nth($i);
+
+                $expandButton = $review->locator('.business-review-view__expand')->first();
+                if ($expandButton->count() > 0) {
+                    $expandButton->focus();
+                    $this->page->keyboard()->press('Enter');
+                }
+
+                $author = $review->locator('.business-review-view__author-name span[itemprop="name"]')
+                    ->textContent();
+                $text = $review->locator('span.spoiler-view__text-container')->textContent();
+
+                $ratingAriaLabel = $review->locator('div.business-rating-badge-view__stars')
+                    ->getAttribute('aria-label');
+                $rating = $this->extractRatingFromAriaLabel($ratingAriaLabel);
+
+                $dateContent = $review->locator('.business-review-view__date meta[itemprop="datePublished"]')
+                    ->getAttribute('content');
+                $date = Carbon::parse($dateContent)->format('Y.m.d');
+
+                $this->reviews[] = compact('author', 'text', 'rating', 'date');
+            }
+        } catch (Exception $e) {
+            $message = "Ошибка при обработке отзывов: " . $e->getMessage();
+            Log::error($message, ['exception' => $e]);
+            throw new Exception($message, 0, $e);
+        }
+    }
+
+    private function extractRatingFromAriaLabel(string $ariaLabel): int
+    {
+        preg_match('/(\d+(?:[.,]\d+)?)/', $ariaLabel, $matches);
+        return (int)$matches[1];
     }
 }
