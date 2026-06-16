7bf392518062ccb5deaf5c76ab70749bad983a9b
kravecil <brightsky@yandex.ru>
2026-06-16 12:29:59 +0500

    feat(routes): реализован редирект на индексную страницу в случае, когда пользовать уже авторизован и пытается войти на страницу аавторизации

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/login', fn() => view('welcome'))->name('login');
+Route::get('/login', function (Request $request) {
+    if ($request->user()) {
+        return redirect()->route('index');
+    }
+
+    return view('welcome');
+})->name('login');
 
 Route::get('/{a?}/{b?}/{c?}', fn() => view('welcome'))
     ->middleware('auth')
